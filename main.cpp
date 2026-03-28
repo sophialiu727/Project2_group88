4c3421a0e6738d7b968fc6fbde382b56f407ae05
patriciadunkelman <paddylin2005@gmail.com>
2026-03-28 19:16:02 +0000

    added console interface

--- main.cpp
+++ main.cpp
@@ -112,6 +112,65 @@ vector<MenuItem> readCSV(const string& filename) {
     return items;
 }
 
+void interface(BTree<stringdata> btree_restaurant, BTree<stringdata> btree_item, RedBlackTree rbtree){
+    int choice = -1;
+    int searchType = 0;
+    while(choice != 0){
+        cout << "\n===== MENU =====\n";
+        cout << "1. Search B-Tree\n";
+        cout << "2. Search Red-Black Tree\n";
+        cout << "0. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        if (choice == 1 || choice == 2) {
+            cout << "\nSearch by:\n";
+            cout << "1. Restaurant name\n";
+            cout << "2. Item name\n";
+            cout << "Enter choice: ";
+            cin >> searchType;
+            cin.ignore();
+        }
+
+        string key;
+        if(choice == 1 || choice == 2){
+            cout << "What would you like to search for?";
+            getline(cin, key);
+            cout << "\nItems Found: \n";
+        }
+        
+        if(choice == 1){
+            if(searchType == 1){
+                auto results = btree_restaurant.search(key);
+                for(int i = 0 ; i < results.size() ; ++i){
+                    results[i] -> printItem();
+                }
+            }
+            if(searchType == 2){
+                auto results = btree_item.searchContains(key);
+                for(int i = 0 ; i < results.size() ; ++i){
+                    results[i] -> printItem();
+                }
+            }
+        }
+        if(choice == 2){
+            if(searchType == 1){
+                auto results = rbtree.search(key);
+                for(int i = 0 ; i < results.size() ; ++i){
+                    results[i].printItem();
+                }
+            }
+            if(searchType == 2){
+                auto results = rbtree.searchContains(key);
+                for(int i = 0 ; i < results.size() ; ++i){
+                    results[i].printItem();
+                }
+            }
+        }
+    }
+}
+
 int main() {
     string filename = "Restaurant_Item_Calories.csv";
 
@@ -137,11 +196,6 @@ int main() {
         btree_restaurant.insert(d);
     }
 
-    // auto seven_eleven = btree_restaurant.search("7 Eleven");
-    // for(int i = 0 ; i < seven_eleven.size() ; ++i){
-    //     cout << seven_eleven[i] -> key << endl;
-    // }
-
     BTree<stringdata> btree_item(items.size());
     for(int i = 0 ; i < items.size() ; ++i){
         stringdata* d = new stringdata;
@@ -150,25 +204,32 @@ int main() {
         btree_item.insert(d);
     }
 
+     RedBlackTree rbtree;
+    for(int i = 0 ; i < items.size() ; ++i){
+        rbtree.insert(items[i]);
+    }
+
+    //  auto seven_eleven = btree_restaurant.search("7 Eleven");
+    // for(int i = 0 ; i < seven_eleven.size() ; ++i){
+    //     cout << seven_eleven[i] -> key << endl;
+    // }
+
     // auto apple_pie = btree_item.searchContains("Bacon");
     // for(int i = 0 ; i < apple_pie.size() ; ++i){
     //     apple_pie[i] -> printItem();
     // }
-
-    RedBlackTree rbtree;
-    for(int i = 0 ; i < items.size() ; ++i){
-        rbtree.insert(items[i]);
-    }
 
     // auto rbtree_7eleven = rbtree.search("7 Eleven");
     // for(int i = 0 ; i < rbtree_7eleven.size() ; ++i){
     //     rbtree_7eleven[i].printItem();
     // }
 
-    auto rbtree_bacon = rbtree.searchContains("Bacon");
-    for(int i = 0 ; i < rbtree_bacon.size() ; ++i){
-        rbtree_bacon[i].printItem();
-    }
+    // auto rbtree_bacon = rbtree.searchContains("Bacon");
+    // for(int i = 0 ; i < rbtree_bacon.size() ; ++i){
+    //     rbtree_bacon[i].printItem();
+    // }
+
+    interface(btree_restaurant, btree_item, rbtree);
 
     return 0;
 }
